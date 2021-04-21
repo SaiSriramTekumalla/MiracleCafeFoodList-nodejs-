6c36c8c085c7eb84c8bab0bd8c5fcdf316b8d066
Tekumalla Sai Sriram <tekumallasaisriram@gmail.com>
2021-04-21 17:14:00 +0530

    Added Format

--- routes/itemsList.js
+++ routes/itemsList.js
@@ -606,7 +606,7 @@ router.post('/deleteCartArray', async (req, res) => {
         // console.log("my orders",myOrders)
 
         let result = await myOrders.save()
-        const timestamp  =  moment(Date.now()).tz("Asia/Kolkata").format().split("+")[0].split("T").join(" ");
+        const timestamp  =  moment(Date.now()).tz("Asia/Kolkata").format("dd/mm/yyyy").split("+")[0].split("T").join(" ");
         response = { result, totalDeductedPoints, message: "Order Placed Successfully" }
         updatedPoints = orderDetails.points - totalDeductedPoints
         await userFavourites.updateOne({ 'employeeID': req.body.employeeID }, { points: updatedPoints });
